bc16fb1730b3d98f38b5263c4c3635281f8aa9ac
Nathan Perry <avaglir@gmail.com>
2017-06-29 18:01:07 -0400

    copy over structs

--- Sources/json.swift
+++ Sources/json.swift
@@ -1,4 +1,47 @@
-struct json {
+public func parseJson(s: Data): JValue {
+    
+}
 
-    var text = "Hello, World!"
+public protocol JValue {
+    toString() -> String
+}
+
+public struct JArray: JValue {
+    var children: [JValue]
+}
+
+public struct JObject: JValue {
+    var elems: [JString:JValue]
+}
+
+public struct JString: JValue, StringLiteralConvertible {
+    var value: String
+
+    init(stringLiteral: String) {
+       	value = stringLiteral
+    }
+}
+
+public struct JNumber: JValue, IntLiteralConvertible, FloatLiteralConvertible {
+    var value: Double
+
+    init(intLiteral: Int) {
+       	value = Double(intLiteral)
+    }
+
+    init(floatLiteral: Float) {
+	value = Double(floatLiteral)
+    }
+}
+
+public struct JBool: JValue, BoolLiteralConvertible {
+    var value: Bool
+
+    init(boolLiteral: Boolean) {
+       	value = boolLiteral
+    }
+}
+
+public struct JNull: JValue, NilLiteralConvertible {
+    init(nilLiteral: ()) {}
 }
