a9574431494e9696da67bfa61fb6dc2ff1d56a4c
Nathan Perry <avaglir@gmail.com>
2017-06-29 22:49:00 -0400

    started on jsmn

--- Sources/parse.swift
+++ Sources/parse.swift
@@ -1,8 +1,19 @@
 import jsmn
 import Foundation
 
-//public func parseJson(s: Data) -> JValue {
+let TOKS_COUNT = 64
 
-    
-    
-//}
+public func parseJson(s: Data) -> Json {
+    let out = try? s.withUnsafeBytes { (pt: UnsafePointer<Int8>) throws -> Json in
+        var parser: jsmn_parser = jsmn_parser()
+        var toks: [jsmntok_t] = [jsmntok_t](repeating: jsmntok_t(), count: TOKS_COUNT)
+        jsmn_init(&parser)
+        jsmn_parse(&parser, pt, s.count, &toks, TOKS_COUNT)
+
+
+        return .null        
+    }
+
+    return out ?? .null
+}
+
--- Tests/jsonTests/jsonTests.swift
+++ Tests/jsonTests/jsonTests.swift
@@ -3,56 +3,56 @@ import XCTest
 
 class jsonTests: XCTestCase {
     static var allTests = [
-        ("testJNumberSerialize", testJNumberSerialize),
-        ("testJStringSerialize", testJStringSerialize),
-        ("testJBoolSerialize", testJBoolSerialize),
-        ("testJNullSerialize", testJNullSerialize),
-        ("testJArraySerialize", testJArraySerialize),
-        ("testJObjectSerialize", testJObjectSerialize),
+        ("testNumberSerialize", testNumberSerialize),
+        ("testStringSerialize", testStringSerialize),
+        ("testBoolSerialize", testBoolSerialize),
+        ("testNullSerialize", testNullSerialize),
+        ("testArraySerialize", testArraySerialize),
+        ("testObjectSerialize", testObjectSerialize),
     ]
 
-    func testJNumberSerialize() {
-        XCTAssertEqual(JNumber(4).jsonString(), "4")
-        XCTAssertEqual(JNumber(0).jsonString(), "0")
-        XCTAssertEqual(JNumber(1).jsonString(), "1")
+    func testNumberSerialize() {
+        XCTAssertEqual(Json.number(4).jsonString(), "4")
+        XCTAssertEqual(Json.number(0).jsonString(), "0")
+        XCTAssertEqual(Json.number(1).jsonString(), "1")
 
-        XCTAssertLessThan(Float(JNumber(4.1).jsonString())! - 4.1, 0.0000001)
-        XCTAssertLessThan(Float(JNumber(0.5).jsonString())! - 0.5, 0.0000001)
-        XCTAssertLessThan(Float(JNumber(1.23515).jsonString())! - 1.23515, 0.0000001)
+        XCTAssertLessThan(Float(Json.number(4.1).jsonString())! - 4.1, 0.0000001)
+        XCTAssertLessThan(Float(Json.number(0.5).jsonString())! - 0.5, 0.0000001)
+        XCTAssertLessThan(Float(Json.number(1.23515).jsonString())! - 1.23515, 0.0000001)
     }
 
-    func testJStringSerialize() {
-        XCTAssertEqual(JString("hello").jsonString(), "\"hello\"")
-        XCTAssertEqual(JString("").jsonString(), "\"\"")
+    func testStringSerialize() {
+        XCTAssertEqual(Json.string("hello").jsonString(), "\"hello\"")
+        XCTAssertEqual(Json.string("").jsonString(), "\"\"")
     }
 
-    func testJBoolSerialize() {
-        XCTAssertEqual(JBool(true).jsonString(), "true")
-        XCTAssertEqual(JBool(false).jsonString(), "false")
+    func testBoolSerialize() {
+        XCTAssertEqual(Json.bool(true).jsonString(), "true")
+        XCTAssertEqual(Json.bool(false).jsonString(), "false")
     }
 
-    func testJNullSerialize() {
-        XCTAssertEqual(JNull().jsonString(), "null")
+    func testNullSerialize() {
+        XCTAssertEqual(Json.null.jsonString(), "null")
     }
 
-    func testJArraySerialize() {
-        let stuff = [JBool(true), JNull(), JString("herp"), JNumber(123), JNumber(-10), JString(""), JObject([JString("herp"): JNumber(345)])]
+    func testArraySerialize() {
+        let stuff = [Json.bool(true), Json.null, Json.string("herp"), Json.number(123), Json.number(-10), Json.string(""), Json.object(["herp": Json.number(345)])]
 
-        let ary = JArray(stuff)
+        let ary = Json.array(stuff)
         
         XCTAssertEqual(ary.jsonString(), "[true,null,\"herp\",123,-10,\"\",{\"herp\":345}]")
     }
 
-    func testJObjectSerialize() {
-        let _ = JObject([
-                            JString("sup"): JNull(),
-                            JString("werp herpderp"): JString("flermpwerp"),
-                            JString("this is an array"): JArray([JNumber(3123),
-                                                                 JNull(),
-                                                                 JBool(false)]),
-                            JString("hello"): JObject([JString("teib do go bobeil"): JNumber(333)]),
-                            JString("yo"): JNumber(123)])
+    func testObjectSerialize() {
+        let _ = Json.object([
+                            "sup": Json.null,
+                            "werp herpderp": Json.string("flermpwerp"),
+                            "this is an array": Json.array([Json.number(3123),
+                                                                 Json.null,
+                                                                 Json.bool(false)]),
+                            "hello": Json.object(["teib do go bobeil": Json.number(333)]),
+                            "yo": Json.number(123)])
         //XCTAssertEqual(obj.jsonString(), "{\"sup\":null,\"werp herpderp\":\"flermpwerp\",\"this is an array\":[3123,null,false],\"hello\":{\"teib do go bobeil\":333},\"yo\":123}")
-        XCTAssertEqual(JObject([:]).jsonString(), "{}")
+        XCTAssertEqual(Json.object([:]).jsonString(), "{}")
     }
 }
